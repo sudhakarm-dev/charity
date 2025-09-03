3dcd1213863d72835b6ddfed3bf9c400596474a8
sudhakarm-dev <msudhakar.dev@gmail.com>
2025-09-03 19:56:52 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-function showcard()
-[
-    
-]
+
+function close_card(){
+    const close=document.getElementById("donation_card");
+    close.style.display = "none";
+}
+function open_card(){
+    const close=document.getElementById("donation_card");
+    close.style.display ="flex";
+}
+
